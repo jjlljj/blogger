a54d31074abdda405f837eb2cd8112d6606d7ceb
jjlljj <jamesl@mba2013-jamesl-2.local>
2016-09-22 13:46:57 -0600

    updated with comments and tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,12 @@
+class TagsController < ApplicationController
+	def show
+		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tags = Tag.all
+	end
+	def destroy
+		@tag = Tag.destroy(params[:id])
+		redirect_to articles_path
+	end
+end
